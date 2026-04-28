985735bafe93ed25cd56203e7f623a809f096303
SebastianValencia-222929 <al222929@alumnos.uacj.mx>
2026-04-28 17:15:09 -0600

    Se agrego el status code 201 a la funcion crear tarea

--- app/routers/team9.py
+++ app/routers/team9.py
@@ -4,17 +4,8 @@ router = APIRouter(prefix="/team9", tags=["Team 9 - Flujo"])
 
 tasks = []  # simulación
 
-@router.post("/workflow/task")
+@router.post("/workflow/task", status_code=201)
 def workflow_task(name: str):
-    # TODO: validar nombre
-    if not name or len(name) < 3:
-        raise HTTPException(status_code=400, detail="Nombre inválido")
-
-    # TODO: evitar duplicados
-    for t in tasks:
-        if t["name"] == name:
-            raise HTTPException(status_code=409, detail="Tarea duplicada")
-
     # Crear tarea
     task = {
         "name": name,
