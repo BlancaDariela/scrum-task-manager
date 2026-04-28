3377b1dfe80a0393f6fefedb9c4fd082bf26de4c
Angel <109712051+ChimoZX@users.noreply.github.com>
2026-04-27 23:44:24 -0600

    "feat: endpoint para tareas completadas"

--- app/routers/team8.py
+++ app/routers/team8.py
@@ -1,6 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.database import SessionLocal
+from app.models import Task
+
 
 router = APIRouter(prefix="/team8", tags=["Team 8 - Reportes"])
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @router.get("/reports/tasks")
 def report_tasks():
@@ -8,9 +20,15 @@ def report_tasks():
     return {"message": "Reporte de tareas"}
 
 @router.get("/reports/completed")
-def report_completed():
-    # TODO: reporte completadas
-    return {"message": "Reporte completadas"}
+def report_completed(db: Session = Depends(get_db)):
+    # Filtrar directamente en la base de datos por el estatus "completed"
+    tareas_completadas = db.query(Task).filter(Task.status == "completed").all()
+
+    return {
+        "message": "Reporte de tareas completadas",
+        "total_completadas": len(tareas_completadas),
+        "data": tareas_completadas
+    }
 
 @router.get("/reports/pending")
 def report_pending():
